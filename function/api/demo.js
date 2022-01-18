c2c416526d09a4b8867bf084ab252b98cce4fbcf
flydu98 <824037006@qq.com>
2022-01-18 22:32:37 +0800

    feat: 使用1.3版本单页模板SDK

--- function/api/demo.js
+++ function/api/demo.js
@@ -1,17 +1,32 @@
 /**
- * 具体的业务函数，在这里实现你发奖，发积分的逻辑，data 入参是固定的，出参必须遵循规范
- * @param { object } data - 业务入参
- * @param { string } data.openId - 当前访问者的微信openId
- * @param { object } data.prize - 中奖的对象
- * @param { string } data.prize.name - 正常奖品记录对象奖品名
- * * @param  cloud - 当前环境的云开发实例
+ * 具体的业务函数，在这里实现你发奖，发积分的逻辑
+ * @param { object } params - 业务入参
+ * @param { string } params.openId - 当前访问者的微信openId
+ * @param  context - 云函数的调用信息和运行状态
  * @returns { object } - 返回参数
  * @returns { number } code 返回的状态标记，成功返回0， 非0代表错误
- * @returns { string } [msg]  如果成功，则可以不返回，如果失败把相应的错误原因中文描述放在这里
- * 
+ * @returns { string } msg 如果成功，则可以不返回，如果失败把相应的错误原因中文描述放在这里
+ * @returns {Object} result 出参必须遵循接口定义
  */
-module.exports = async (data, cloud) => {
-    console.log("参数：", data);
-    // 这里实现你具体的业务逻辑，比如发积分，发奖
-    return {code:0, msg:'suc'};
-};
+
+const {
+    cloudbase
+} = require('@cloudbase/page-module')
+
+module.exports = async function doSomething(params, context) {
+    console.log(params, context, 'doSomething')
+
+    //   调用当前环境的云函数
+    //   const result = await cloudbase.callFunction({
+    //     name: 'doSomething',
+    //   })
+
+    // 出参需要遵守自定接口规范
+    return {
+        code: 0,
+        msg: 'success',
+        result: {
+            sendResult: true
+        }
+    };
+}
